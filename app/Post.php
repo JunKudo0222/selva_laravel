c5afd5e242aae856862af7cc6489a85188031406
kudoujun <kudoujun@kudoujunnoMacBook-Air.local>
2021-09-27 14:53:17 +0900

    migration

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,34 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Post extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+    protected $fillable = ['name', 'product_content','user_id','product_category_id','product_subcategory_id','image1','image2','image3','image4'];
+
+    // public function comments()
+    // {
+    //     return $this->hasMany('App\Comment');
+    // }
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    public function product_category()
+    {
+        return $this->belongsTo('App\Product_category');
+    }
+    public function product_subcategory()
+    {
+        return $this->belongsTo('App\Product_subcategory');
+    }
+    public function users()
+    {
+        return $this->belongsToMany('App\User')->withTimestamps();
+    }
 }
--- database/migrations/2021_09_28_091252_create_posts_table.php
+++ database/migrations/2021_09_28_091252_create_posts_table.php
@@ -17,9 +17,18 @@ class CreatePostsTable extends Migration
             $table->bigIncrements('id');
             $table->timestamps();
             $table->unsignedBigInteger('user_id');
-            $table->string('title');
-            $table->string('body');
+            $table->unsignedBigInteger('product_category_id');
+            $table->unsignedBigInteger('product_subcategory_id');
+            $table->string('name');
+            $table->string('product_content');
             $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('product_category_id')->references('id')->on('product_categories');
+            $table->foreign('product_subcategory_id')->references('id')->on('product_subcategories');
+
+            $table->string('image1');
+            $table->string('image2');
+            $table->string('image3');
+            $table->string('image4');
         });
     }
 
