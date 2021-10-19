9514a12eeb4039a213cdc7487960b648909c8824
kudoujun <kudoujun@kudoujunnoMacBook-Air.local>
2021-10-19 21:01:06 +0900

    post request

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -24,8 +24,8 @@ class PostRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'=>'required|max:20',
-            'product_content'=>'required',
+            'name'=>'required|max:100',
+            'product_content'=>'required|max:500',
             'product_category'=>'required|between:1,5',
             'product_subcategory'=>'required|between:1,25',
             
@@ -36,7 +36,8 @@ class PostRequest extends FormRequest
 {
     return [
         'name.required' => '商品名は必須です。',
-    'name.max'      => '商品名は20文字以内で記入してください。',
+    'name.max'      => '商品名は100文字以内で記入してください。',
+    'product_content.max'      => '内容は500文字以内で記入してください。',
         'product_content.required'  => '内容は必須です。',
         
         
